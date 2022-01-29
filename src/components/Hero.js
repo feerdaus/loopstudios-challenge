df535415887e717f958f6a8dc0c04b5bb83e1b08
feerdaus <firdauslawal@gmail.com>
2022-01-29 13:27:11 +0100

    Add assets

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-title">Immersive experiences that deliver</div>
+    </section>
+  );
+};
+
+export default Hero;
